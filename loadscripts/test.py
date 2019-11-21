c88a32447f48fe7b6785d5307518c5569671d687
roblpm <roblpm@hotmail.com>
2019-11-21 11:37:14 +0000

    Thursday Lunch

--- loadscripts/test.py
+++
@@ -1,4 +0,0 @@
-from numpy import genfromtxt
-file_name = "../../data/world_happiness.csv"
-data = genfromtxt(file_name, delimiter=',', skip_header=1, converters={0: lambda s: str(s)})
-print(type(data))
