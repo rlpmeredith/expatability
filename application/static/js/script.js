dfb4430a92be253d86fe53b65a522a324712ba7c
roblpm <roblpm@hotmail.com>
2019-12-01 12:05:50 +0000

    Sunday Noon

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -23,7 +23,7 @@ const Questions = new Vue({
        vuetify: new Vuetify(),
        data: {
             cards: [
-                { title: 'Does the amount of sunlight in a day matter to you?', src: './static/images/Valletta.jpg', flex: 4 },
+                { title: 'Does the amount of sunlight in a day matter to you?', src: './static/images/Valletta.jpg', flex: 4, sortorder: 'daylength' },
                 { title: 'Do you want to see if a place ranks high in acceptance of immigrants?', src: './static/images/paris.jpg', flex: 4 },
                 { title: 'How about looking at cities ranked by cost of living?', src: './static/images/London.jpg', flex: 4 },
                 { title: "Do you want to narrow your search based on a city's average temperature?", src: './static/images/Stockholm.jpg', flex: 4 },
@@ -31,6 +31,19 @@ const Questions = new Vue({
                 { title: 'Or would you like to search for a particular city?', src: './static/images/sign.jpg', flex: 4 },
             ],
       },
+      methods: {
+            proceed() {
+                console.log('Hello')
+            },
+            get_data(sortorder) {
+//                const url = `api/citylist?order=${sortorder}`
+                console.log(${sortorder})
+//                fetch(url, {
+//                method: 'GET'
+//                    }).then(res => res.json())
+//                    .then((citydata) => this.happinessTable = citydata)
+            }
+      }
 })
 
 
